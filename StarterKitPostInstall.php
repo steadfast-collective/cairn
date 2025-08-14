3fe707e66f9d7f800b91625db2092b1cdc4341a8
James Pigott <james.pigott@steadfastcollective.com>
2025-08-14 12:47:10 +0100

    updates

--- StarterKitPostInstall.php
+++ StarterKitPostInstall.php
@@ -1,11 +1,40 @@
 <?php
 
+use Symfony\Component\Process\Process;
+use function Laravel\Prompts\confirm;
 class StarterKitPostInstall
 {
     public function handle($console)
     {
-        $this->run(
-            command: 'php artisan config:clear'
-        );
+        $console->call('optimize:clear');
+
+        $this->installNpmDependencies($console);
+    }
+
+    protected function installNpmDependencies($console)
+    {
+
+        if (! confirm('Do you want to install NPM dependencies?')) {
+            return;
+        }
+
+        $this->runShellCommand('npm install', $console);
+    }
+
+    private function runShellCommand($command, $console)
+    {
+        $console->info("Running: {$command}");
+
+        $process = Process::fromShellCommandline($command);
+        $process->setTimeout(300); // 5 minutes timeout
+
+        $process->run(function ($type, $buffer) use ($console) {
+            $console->info($buffer);
+        });
+
+        if (!$process->isSuccessful()) {
+            $console->error("Command failed: {$command}");
+            $console->error($process->getErrorOutput());
+        }
     }
 }
--- export/README.example.md
+++ export/README.example.md
@@ -4,7 +4,8 @@
 1. Setup a new Statamic site using the Statamic CLI `statamic new`
 2. You'll be asked if you want to use a starter kit. Say no to this.
 3. Go through the Statamic install process as per usual.
-4. Once complete, add the following to your `composer.json`, (This is because a couple of packages we're using aren't published to packagist.)
+4. Make sure a mySQL database is setup, and the correct details are in your env
+5. Once complete, add the following to your `composer.json`, (This is because a couple of packages we're using aren't published to packagist.)
 ```
 "repositories": [
     {
@@ -17,8 +18,8 @@
     }
 ]
 ```
-5. Run `php please starter-kit:install steadfast-collective/cairn`
-6. When asked `Clear Site First?`, choose yes.
-9. Create a super user when prompted. 
-7. Run `npm install`.
-8. Run `npm run dev` or `npm run build`.
+6. Run `php please starter-kit:install steadfast-collective/cairn`
+7. When asked `Clear Site First?`, choose yes.
+8. Create a super user when prompted. 
+9. Run `npm install`.
+10. Run `npm run dev` or `npm run build`.
